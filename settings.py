c118dbb755a815c5b25131937523fb613bc690b9
Rossi K <rkitgreen007@gmail.com>
2026-06-09 18:29:47 +0200

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,24 @@
+# myproject/settings.py (Modify or append these lines)
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'], # Tell Django to check root /templates folder
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Allow Chainlit iframe to render if hosted on the same domain
+X_FRAME_OPTIONS = 'SAMEORIGIN'
